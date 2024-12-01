0b1fc83cad8d242f83438b10daa07315b35e0f48
Biagiarchiefz <biagiiarchie@gmail.com>
2024-12-01 14:26:01 +0700

    menggunakan package npm

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,15 @@
-console.log('Hello Biagi')
+// console.log('Hello Biagi')
+import chalk from 'chalk';
+
+// const validator = require('validator');
+// const chalk = require('chalk');
+
+// console.log ( validator.isEmail('Bigi@gmail.com') )
+
+// console.log ( validator.isMobilePhone('08523323233', 'id-ID') )
+
+
+
+console.log(chalk.red('Hello Biagi'))
+console.log(chalk.bgBlue.black.bold('Hello world!'));
+
