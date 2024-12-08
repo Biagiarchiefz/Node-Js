e24884ee8801263d71ca167ac8f6c14badd9c6e3
Biagiarchiefz <biagiiarchie@gmail.com>
2024-12-08 23:42:18 +0700

    web server

---
+++ 13-Nodejs-web-sever/app.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const fs = require('fs')
+
+
+const renderHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.writeHead('Error: file tidak ditemukan')
+    } else {
+      res.write(data)
+    }
+    res.end()
+  })
+}
+
+const server = http.createServer( (req, res ) => {
+res.writeHead(200, {
+    'Content-Type' : 'text/html',
+   });
+
+   const url = req.url;
+
+   if ( url === '/about'){
+    renderHTML('./about.html', res)
+
+   } else if ( url === '/contact') {
+    res.write('ini halaman contact')
+    res.end()
+
+   } else {
+    //  res.write('Halaman ini tidak ditemukan ');
+    renderHTML('./index.html', res)
+  }
+})
+
+
+server.listen(3000, () => {
+  console.log('server is listening on port 3000')    // pesan pada saat program berjalan 
+})
