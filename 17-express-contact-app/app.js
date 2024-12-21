e99342eab63e7a70cf0ab8aac2d3108b1031e40b
Biagiarchiefz <biagiiarchie@gmail.com>
2024-12-22 01:00:31 +0700

    express contact part 1

--- 17-express-contact-app/app.js
+++ 17-express-contact-app/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
-const { loadContact } = require('./utils/contacts')
+const { loadContact, findContact } = require('./utils/contacts')
 
 const app = express()
 const port = 3000
@@ -65,6 +65,16 @@ app.get('/contact', (req, res) => {
     layout: 'layouts/main-layout', 
     title : 'Halaman contact',
     contacts
+    })
+})
+
+app.get('/contact/:nama', (req, res) => {  
+  const contact = findContact(req.params.nama)
+
+  res.render('detail', {
+    layout: 'layouts/main-layout', 
+    title : 'Halaman detail contact',
+    contact
     })
 })
 
--- 17-express-contact-app/utils/contacts.js
+++ 17-express-contact-app/utils/contacts.js
@@ -16,11 +16,19 @@ if (!fs.existsSync(filePath) ) {          // jika filePath tidak ada kalau true 
   fs.writeFileSync(filePath, '[]' , 'utf-8')       
 }
 
-
+// ambil semua data di contact.json 
 const loadContact = () => {
   const file = fs.readFileSync ('data/contact.json', 'utf-8')
   const contacts = JSON.parse(file)    // untuk merubah string menjadi JSON menggunakan parse
   return contacts 
 }
 
-module.exports = {loadContact}
+// cari contact berdasarkan nama
+const findContact = (nama) => {
+  const contacts = loadContact();    // loadContact() ambil semua data contact
+
+  const contact = contacts.find( (contact) => contact.nama.toLowerCase() === nama.toLowerCase() )
+  return contact
+}
+
+module.exports = {loadContact, findContact}
